d53dbbe152f76e58b5222093db0a6082b998e7ba
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-04 02:16:32 +0700

    HW3

--- minMedMax.js
+++ minMedMax.js
@@ -1,7 +1,7 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  const mValue = [num1,num2,num3]
+  const mValue = [n1,n2,n3]
   let maxValue = mValue[0]
   let minValue = mValue[0]
   let midValue = mValue[0]
@@ -21,14 +21,14 @@ function minMedMax(n1, n2, n3) {
     if(Value != midValue && Value != maxValue){
       midValue = Value
     }
-    if(num1 === num2){
-      midValue = num1
+    if(n1 === n2){
+      midValue = n1
     }
-    if(num1 === num3){
-      midValue = num1
+    if(n1 === n3){
+      midValue = n1
     }
-    if(num2 === num3){
-      midValue = num2
+    if(n2 === n3){
+      midValue = n2
     }
   }
 return {min: minValue, mid: midValue, max: maxValue}
