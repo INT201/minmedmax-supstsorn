36b190d07962030b8e86e6b9a2401c00d38667cf
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-04 02:56:42 +0700

    HW3

--- minMedMax.js
+++ minMedMax.js
@@ -1,37 +1,40 @@
 const { template } = require('@babel/core')
 
-function minMedMax(n1, n2, n3) {
+function minMedMax(n1,n2,n3){
   const mValue = [n1,n2,n3]
   let maxValue = mValue[0]
   let minValue = mValue[0]
   let midValue = mValue[0]
 
+  // find Max Value
   for(const value of mValue){
-    if(value > maxValue){
-      maxValue = value;
-    }
+      if(value > maxValue){
+          maxValue = value;
+      }
   }
+  // find Min value
+  for(const value of mValue){
+      if(value < minValue){
+          minValue = value;
+      }
+  }
+// find mid value
+  for(const value of mValue){
+      if(value != minValue && value != maxValue){
+          midValue = value
+      }
+      if(n1 === n2){
+          midValue = num1
+      }
 
-  for(const value of mValue){
-    if(value < minValue){
-      minValue = value;
-    }
-  }
+      if(n1 === n3){
+          midValue = num1
+      }
 
-  for(const value of mValue){
-    if(value != minValue && value != maxValue){
-      midValue = value
-    }
-    if(n1 === n2){
-      midValue = n1
-    }
-    if(n1 === n3){
-      midValue = n1
-    }
-    if(n2 === n3){
-      midValue = n2
-    }
+      if(n2 === n3){
+          midValue = num2
+      }
   }
-return {min: minValue, mid: midValue, max: maxValue}
+  return {min: minValue, mid: midValue, max: maxValue}
 }
 module.exports = minMedMax
